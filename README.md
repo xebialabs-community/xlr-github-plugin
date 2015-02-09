18bdef7d66794c8ccfa9040db363834d123b584b
Bulat Yaminov <byaminov@xebialabs.com>
2015-02-09 08:52:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# [DRAFT] XL Release GitHub Plugin #
+
+[![Build Status](https://travis-ci.org/xebialabs-community/xlr-github-plugin.svg?branch=master)](https://travis-ci.org/xebialabs-community/xlr-github-plugin)
+
+The `xlr-github-plugin` is an [XL Release](https://docs.xebialabs.com/xl-release/index.html) plugin that allows you to integrate with GitHub.
+
+**NOTE**: this plugin depends on the `xlr-git-plugin` which is shipped in standard XL Release distribution.
+
+## Tasks ##
+
+**TBD**
---
+++ src/main/java/com/xebialabs/xlrelease/plugin/git/MergeFailedException.java
@@ -0,0 +1,17 @@
+/**
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS
+ * FOR A PARTICULAR PURPOSE. THIS CODE AND INFORMATION ARE NOT SUPPORTED BY XEBIALABS.
+ */
+package com.xebialabs.xlrelease.plugin.git;
+
+/**
+ * Exception thrown when automatic merge of branches cannot be done.
+ */
+public class MergeFailedException extends RuntimeException {
+
+    public MergeFailedException(String message) {
+        super(message);
+    }
+
+}
