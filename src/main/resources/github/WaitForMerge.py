0994bf7af9056a22f79b0a860bc83e5cdecdc18d
Sushant K Pradhan (TMNA) <sushant.pradhan@toyota.com>
2020-04-17 20:13:57 -0500

    Fix for #19 Use task.schedule instead of time.sleep

--- src/main/resources/github/WaitForMerge.py
+++ src/main/resources/github/WaitForMerge.py
@@ -5,18 +5,14 @@ from github import GithubException
 g_client = GithubClient(server)
 g = g_client.get_github_client(locals())
 repo = g_client.get_repo(g, organization, repositoryName)
-# Call the function to check PR is merged
-isMerged = False
-while (not isMerged):
-	pr_details = repo.get_pull(prid)
-	pr_merged = pr_details.merged
-	pr_state = pr_details.state
-	if pr_state == "closed":
-		if pr_merged == True:
-			isMerged = True
-			print "PR merged and closed"
-		elif pr_merged == False: # PR is closed without being merged
-			print "PR closed without merge"
-			exit(1)
-	else:
-		time.sleep(pollInterval)
+pr_details = repo.get_pull(prid)
+pr_merged = pr_details.merged
+pr_state = pr_details.state
+if pr_state == "closed":
+    if pr_merged == True:
+        task.setStatusLine("Success")
+    elif pr_merged == False: # PR is closed without being merged
+        raise Exception("[PR %s] was closed without merge " % (prid))
+else:
+    task.setStatusLine("Waiting for PR to be merged...")
+    task.schedule("github/WaitForMerge.py", pollInterval)
